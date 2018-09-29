3c8c38b6b857cb83431c464bcfbd7f1c40b1b251
Herman Liang <hermanliang80@gmail.com>
2018-09-29 07:15:14 -0600

    kata #4: Median of Two Sorted Arrays

---
+++ src/main/java/me/hermanliang/kata/array/SortedArray.java
@@ -0,0 +1,23 @@
+package me.hermanliang.kata.array;
+
+/**
+ * @see <a href="https://leetcode.com/problems/median-of-two-sorted-arrays/description/">
+ * https://leetcode.com/problems/median-of-two-sorted-arrays/description/</a>
+ */
+public class SortedArray {
+
+    /**
+     * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+     * Find the median of the two sorted arrays. The overall run time complexity
+     * should be O(log (m+n)).
+     * You may assume nums1 and nums2 cannot be both empty.
+     *
+     * @param nums1 nums1
+     * @param nums2 nums2
+     * @return median of the two sorted arrays
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/me/hermanliang/kata/array/SortedArrayTest.java
@@ -0,0 +1,23 @@
+package me.hermanliang.kata.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SortedArrayTest {
+
+    @Test
+    public void findMedianSortedArrays1() {
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        assertEquals(2.0, new SortedArray().findMedianSortedArrays(nums1, nums2), 0.1);
+    }
+
+    @Test
+    public void findMedianSortedArrays2() {
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3, 4};
+        assertEquals(2.5, new SortedArray().findMedianSortedArrays(nums1, nums2), 0.1);
+    }
+
+}
