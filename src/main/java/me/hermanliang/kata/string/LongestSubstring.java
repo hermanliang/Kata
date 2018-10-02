7112c586817db0f4211975ce028d12e2f070c34c
Herman Liang <hermanliang80@gmail.com>
2018-10-02 07:16:49 -0600

    kata #5: solution 3.02%

--- src/main/java/me/hermanliang/kata/string/LongestSubstring.java
+++ src/main/java/me/hermanliang/kata/string/LongestSubstring.java
@@ -42,6 +42,33 @@ public class LongestSubstring {
      * @return longest palindromic substring
      */
     public String longestPalindrome(String input) {
-        return null;
+        int len = input.length();
+        String ans = "";
+        int maxLen = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j <= len; j++) {
+                int subLen = j - i;
+                if (subLen > maxLen) {
+                    String subString = input.substring(i, j);
+                    if (isPalindrome(subString)) {
+                        if (subLen > maxLen) {
+                            maxLen = subLen;
+                            ans = subString;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    private boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right) {
+            if (s.charAt(left++) != s.charAt(right--))
+                return false;
+        }
+        return true;
     }
 }
