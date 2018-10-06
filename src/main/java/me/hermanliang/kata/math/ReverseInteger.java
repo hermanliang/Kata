3790e786d02364f0219a76cc71b06a72bc3045cc
Herman Liang <hermanliang80@gmail.com>
2018-10-06 08:19:06 -0600

    kata #7: Reverse integer

---
+++ src/main/java/me/hermanliang/kata/math/ReverseInteger.java
@@ -0,0 +1,19 @@
+package me.hermanliang.kata.math;
+
+/**
+ * @see <a href="https://leetcode.com/problems/reverse-integer/description/">
+ * https://leetcode.com/problems/reverse-integer/description/</a>
+ */
+public class ReverseInteger {
+
+    /**
+     * Given a 32-bit signed integer, reverse digits of an integer.
+     *
+     * @param input input integer
+     * @return reversed integer
+     */
+    public int reverse(int input) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/me/hermanliang/kata/math/ReverseIntegerTest.java
@@ -0,0 +1,30 @@
+package me.hermanliang.kata.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReverseIntegerTest {
+
+    @Test
+    public void reverse1() {
+        int input = 123;
+        int expected = 321;
+        assertEquals(expected, new ReverseInteger().reverse(input));
+    }
+
+    @Test
+    public void reverse2() {
+        int input = -123;
+        int expected = -321;
+        assertEquals(expected, new ReverseInteger().reverse(input));
+    }
+
+    @Test
+    public void reverse3() {
+        int input = 120;
+        int expected = 21;
+        assertEquals(expected, new ReverseInteger().reverse(input));
+    }
+
+}
