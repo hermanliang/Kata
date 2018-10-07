5c6a137e9e63a73209733bc3ca5c3230e62a60a5
Herman Liang <hermanliang80@gmail.com>
2018-10-07 06:19:50 -0600

    kata #7: solution 15.57%

--- src/main/java/me/hermanliang/kata/math/ReverseInteger.java
+++ src/main/java/me/hermanliang/kata/math/ReverseInteger.java
@@ -13,7 +13,21 @@ public class ReverseInteger {
      * @return reversed integer
      */
     public int reverse(int input) {
-        return 0;
+        long result = 1;
+        String digits = String.valueOf(input);
+        StringBuilder container = new StringBuilder();
+        if (digits.charAt(0) == '-') {
+            result = -1;
+            digits = digits.substring(1);
+        }
+        for (int i = digits.toCharArray().length - 1; i >= 0; i--) {
+            container.append(digits.charAt(i));
+        }
+        result *= Long.parseLong(container.toString());
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) result;
     }
 
 }
