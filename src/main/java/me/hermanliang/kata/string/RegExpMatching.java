5108e80d4b88af14c06c02a36c33648a50a05ba6
Herman Liang <hermanliang80@gmail.com>
2018-10-13 06:23:42 -0600

    kata #10: Regular Expression Matching

---
+++ src/main/java/me/hermanliang/kata/string/RegExpMatching.java
@@ -0,0 +1,30 @@
+package me.hermanliang.kata.string;
+
+/**
+ * @see <a href="https://leetcode.com/problems/regular-expression-matching/description/">
+ * https://leetcode.com/problems/regular-expression-matching/description/</a>
+ */
+public class RegExpMatching {
+
+    /**
+     * 10. Regular Expression Matching
+     * <p>
+     * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+     * <p>
+     * '.' Matches any single character.
+     * '*' Matches zero or more of the preceding element.
+     * <p>
+     * The matching should cover the entire input string (not partial).
+     * <p>
+     * Note:
+     * s could be empty and contains only lowercase letters a-z.
+     * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+     *
+     * @param s input string
+     * @param p pattern
+     * @return is match or not
+     */
+    public boolean isMatch(String s, String p) {
+        return false;
+    }
+}
