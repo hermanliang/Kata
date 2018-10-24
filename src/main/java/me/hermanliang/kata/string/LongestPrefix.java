946b577db4fa8a38f5b6aeae294415e458c4c9b6
Herman Liang <hermanliang80@gmail.com>
2018-10-24 06:30:33 -0600

    kata #14: Longest Common Prefix

---
+++ src/main/java/me/hermanliang/kata/string/LongestPrefix.java
@@ -0,0 +1,24 @@
+package me.hermanliang.kata.string;
+
+/**
+ * @see <a href="https://leetcode.com/problems/longest-common-prefix/description/">
+ * https://leetcode.com/problems/longest-common-prefix/description/</a>
+ */
+public class LongestPrefix {
+
+    /**
+     * 14. Longest Common Prefix
+     * <p>
+     * Write a function to find the longest common prefix string amongst an array of strings.
+     * If there is no common prefix, return an empty string "".
+     * <p>
+     * Note:
+     * All given inputs are in lowercase letters a-z.
+     *
+     * @param strs array of strings
+     * @return longest common prefix
+     */
+    public String longestCommonPrefix(String[] strs) {
+        return null;
+    }
+}
---
+++ src/test/java/me/hermanliang/kata/string/LongestPrefixTest.java
@@ -0,0 +1,21 @@
+package me.hermanliang.kata.string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LongestPrefixTest {
+
+    @Test
+    public void longestCommonPrefix1() {
+        String[] strs = {"flower", "flow", "flight"};
+        assertEquals("fl", new LongestPrefix().longestCommonPrefix(strs));
+    }
+
+    @Test
+    public void longestCommonPrefix2() {
+        String[] strs = {"dog", "racecar", "car"};
+        // There is no common prefix among the input strings
+        assertEquals("", new LongestPrefix().longestCommonPrefix(strs));
+    }
+}
