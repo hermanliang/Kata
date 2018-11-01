bc90872f3c41bb1eacc023bdd7693604e2279873
Herman Liang <hermanliang80@gmail.com>
2018-11-01 06:36:24 -0600

    kata #17: Letter Combinations of a Phone Number

---
+++ src/main/java/me/hermanliang/kata/string/LetterCombination.java
@@ -0,0 +1,34 @@
+package me.hermanliang.kata.string;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/">
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/</a>
+ */
+public class LetterCombination {
+
+    /**
+     * 17. Letter Combinations of a Phone Number [Medium]
+     * <p>
+     * Given a string containing digits from 2-9 inclusive, return all possible letter combinations
+     * that the number could represent.
+     * A mapping of digit to letters (just like on the telephone buttons) is given below.
+     * Note that 1 does not map to any letters.
+     * <p>
+     * Example:
+     * <p>
+     * Input: "23"
+     * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+     * <p>
+     * Note:
+     * <p>
+     * Although the above answer is in lexicographical order, your answer could be in any order you want.
+     *
+     * @param digits digits
+     * @return all possible letter combinations
+     */
+    public List<String> letterCombinations(String digits) {
+        return null;
+    }
+}
