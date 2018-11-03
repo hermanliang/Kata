b8d03be72af378be5e0f1497012f4d0b369ac39c
Herman Liang <hermanliang80@gmail.com>
2018-11-03 06:36:58 -0600

    kata #18: 4Sum

---
+++ src/main/java/me/hermanliang/kata/array/FourSum.java
@@ -0,0 +1,39 @@
+package me.hermanliang.kata.array;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/4sum/description/">
+ * https://leetcode.com/problems/4sum/description/</a>
+ */
+public class FourSum {
+
+    /**
+     * 18. 4Sum [Medium]
+     * <p>
+     * Given an array nums of n integers and an integer target, are there elements a, b, c, and d
+     * in nums such that a + b + c + d = target? Find all unique quadruplets in the array
+     * which gives the sum of target.
+     * <p>
+     * Note:
+     * The solution set must not contain duplicate quadruplets.
+     * <p>
+     * Example:
+     * <p>
+     * Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+     * <p>
+     * A solution set is:
+     * [
+     * [-1,  0, 0, 1],
+     * [-2, -1, 1, 2],
+     * [-2,  0, 0, 2]
+     * ]
+     *
+     * @param nums   number array
+     * @param target target
+     * @return all unique quadruplets in the array which gives the sum of target
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        return null;
+    }
+}
---
+++ src/test/java/me/hermanliang/kata/array/FourSumTest.java
@@ -0,0 +1,43 @@
+package me.hermanliang.kata.array;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FourSumTest {
+
+    @Test
+    public void fourSum() {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        List<List<Integer>> output = new FourSum().fourSum(nums, target);
+
+        List<Integer> expected1 = Arrays.asList(-1, 0, 0, 1);
+        List<Integer> expected2 = Arrays.asList(-2, -1, 1, 2);
+        List<Integer> expected3 = Arrays.asList(-2, 0, 0, 2);
+        assertEquals(3, output.size());
+        List<Integer> list1 = new ArrayList<>(output.get(0));
+        List<Integer> list2 = new ArrayList<>(output.get(1));
+        List<Integer> list3 = new ArrayList<>(output.get(2));
+        Collections.sort(list1);
+        Collections.sort(list2);
+        Collections.sort(list3);
+        assertNotEquals(list1, list2);
+        assertNotEquals(list2, list3);
+        assertNotEquals(list1, list3);
+        assertTrue(list1.equals(expected1) ||
+                list1.equals(expected2) ||
+                list1.equals(expected3));
+        assertTrue(list2.equals(expected1) ||
+                list2.equals(expected2) ||
+                list2.equals(expected3));
+        assertTrue(list3.equals(expected1) ||
+                list3.equals(expected2) ||
+                list3.equals(expected3));
+    }
+}
