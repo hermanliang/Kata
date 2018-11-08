003326cdd38d50a0a8f935bca10ad096b6a96697
Herman Liang <hermanliang80@gmail.com>
2018-11-08 06:38:15 -0600

    kata #20: Valid Parentheses

---
+++ src/main/java/me/hermanliang/kata/string/ValidParentheses.java
@@ -0,0 +1,27 @@
+package me.hermanliang.kata.string;
+
+/**
+ * @see <a href="https://leetcode.com/problems/valid-parentheses/description/">
+ * https://leetcode.com/problems/valid-parentheses/description/</a>
+ */
+public class ValidParentheses {
+
+    /**
+     * 20. Valid Parentheses [Easy]
+     * <p>
+     * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+     * determine if the input string is valid.
+     * <p>
+     * An input string is valid if:
+     * <p>
+     * Open brackets must be closed by the same type of brackets.
+     * Open brackets must be closed in the correct order.
+     * Note that an empty string is also considered valid.
+     *
+     * @param s input string
+     * @return is valid parentheses or not
+     */
+    public boolean isValid(String s) {
+        return false;
+    }
+}
---
+++ src/test/java/me/hermanliang/kata/string/ValidParenthesesTest.java
@@ -0,0 +1,39 @@
+package me.hermanliang.kata.string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ValidParenthesesTest {
+
+    @Test
+    public void isValid1() {
+        String input = "()";
+        assertTrue(new ValidParentheses().isValid(input));
+    }
+
+    @Test
+    public void isValid2() {
+        String input = "()[]{}";
+        assertTrue(new ValidParentheses().isValid(input));
+    }
+
+    @Test
+    public void isValid3() {
+        String input = "(]";
+        assertFalse(new ValidParentheses().isValid(input));
+    }
+
+    @Test
+    public void isValid4() {
+        String input = "([)]";
+        assertFalse(new ValidParentheses().isValid(input));
+    }
+
+    @Test
+    public void isValid5() {
+        String input = "{[]}";
+        assertTrue(new ValidParentheses().isValid(input));
+    }
+}
