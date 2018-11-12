853fa3de0ca6c09f873c6e9a56b82a581729715d
Herman Liang <hermanliang80@gmail.com>
2018-11-12 06:39:14 -0600

    kata #22: Generate Parentheses

---
+++ src/main/java/me/hermanliang/kata/string/GenerateParentheses.java
@@ -0,0 +1,32 @@
+package me.hermanliang.kata.string;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/generate-parentheses/description/">
+ * https://leetcode.com/problems/generate-parentheses/description/</a>
+ */
+public class GenerateParentheses {
+
+    /**
+     * 22. Generate Parentheses [Medium]
+     * <p>
+     * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+     * <p>
+     * For example, given n = 3, a solution set is:
+     * <p>
+     * [
+     * "((()))",
+     * "(()())",
+     * "(())()",
+     * "()(())",
+     * "()()()"
+     * ]
+     *
+     * @param n pairs of parentheses
+     * @return list of well-formed parentheses
+     */
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+}
---
+++ src/test/java/me/hermanliang/kata/string/GenerateParenthesesTest.java
@@ -0,0 +1,31 @@
+package me.hermanliang.kata.string;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GenerateParenthesesTest {
+
+    @Test
+    public void generateParenthesis() {
+        List<String> expected = Arrays.asList(
+                "((()))",
+                "(()())",
+                "(())()",
+                "()(())",
+                "()()()"
+        );
+        List<String> output = new GenerateParentheses().generateParenthesis(3);
+        Set<String> dedup = new HashSet<>(output);
+        assertEquals(expected.size(), dedup.size());
+        for (String s : dedup) {
+            assertTrue(expected.contains(s));
+        }
+    }
+}
