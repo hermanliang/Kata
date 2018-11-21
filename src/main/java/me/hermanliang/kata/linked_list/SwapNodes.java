f98b7fcea86aea4add8540057507fb37c0d245e7
Herman Liang <hermanliang80@gmail.com>
2018-11-21 06:41:24 -0600

    kata #25: Reverse Nodes in k-Group

--- src/main/java/me/hermanliang/kata/linked_list/SwapNodes.java
+++ src/main/java/me/hermanliang/kata/linked_list/SwapNodes.java
@@ -5,6 +5,8 @@ import me.hermanliang.kata.util.ListNode;
 /**
  * @see <a href="https://leetcode.com/problems/swap-nodes-in-pairs/description/">
  * https://leetcode.com/problems/swap-nodes-in-pairs/description/</a>
+ * @see <a href="https://leetcode.com/problems/reverse-nodes-in-k-group/description/">
+ * https://leetcode.com/problems/reverse-nodes-in-k-group/description/</a>
  */
 public class SwapNodes {
 
@@ -35,5 +37,34 @@ public class SwapNodes {
             cursor = cursor.next.next;
         }
         return head;
+    }
+
+    /**
+     * 25. Reverse Nodes in k-Group
+     * <p>
+     * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+     * <p>
+     * k is a positive integer and is less than or equal to the length of the linked list.
+     * If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+     * <p>
+     * Example:
+     * <p>
+     * Given this linked list: 1->2->3->4->5
+     * <p>
+     * For k = 2, you should return: 2->1->4->3->5
+     * <p>
+     * For k = 3, you should return: 3->2->1->4->5
+     * <p>
+     * Note:
+     * <p>
+     * Only constant extra memory is allowed.
+     * You may not alter the values in the list's nodes, only nodes itself may be changed.
+     *
+     * @param head a list
+     * @param k    reverse k nodes
+     * @return result
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        return null;
     }
 }
--- src/test/java/me/hermanliang/kata/linked_list/SwapNodesTest.java
+++ src/test/java/me/hermanliang/kata/linked_list/SwapNodesTest.java
@@ -20,4 +20,19 @@ public class SwapNodesTest {
         }
         assertNull(output);
     }
+
+    @Test
+    public void reverseKGroup() {
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] expected = {3, 2, 1, 4, 5};
+        int k = 3;
+        ListNode head = ListNode.arrayToListNode(nums);
+        ListNode output = new SwapNodes().reverseKGroup(head, k);
+        for (int num : expected) {
+            assertNotNull(output);
+            assertEquals(num, output.val);
+            output = output.next;
+        }
+        assertNull(output);
+    }
 }
