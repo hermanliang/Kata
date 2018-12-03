9ceef65101b2b3b77ab74c65b7746aa8261cfe78
Herman Liang <hermanliang80@gmail.com>
2018-12-03 07:13:30 -0600

    kata #38: Count and Say

---
+++ src/test/java/me/hermanliang/kata/string/CountAndSayTest.java
@@ -0,0 +1,22 @@
+package me.hermanliang.kata.string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountAndSayTest {
+
+    @Test
+    public void countAndSay1() {
+        int n = 1;
+        String expected = "1";
+        assertEquals(expected, new CountAndSay().countAndSay(n));
+    }
+
+    @Test
+    public void countAndSay2() {
+        int n = 4;
+        String expected = "1211";
+        assertEquals(expected, new CountAndSay().countAndSay(n));
+    }
+}
