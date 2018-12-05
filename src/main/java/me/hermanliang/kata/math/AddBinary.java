50b7dc55c280f3bb5e9a2d798df71690dbe7edaa
Herman Liang <hermanliang80@gmail.com>
2018-12-05 07:35:35 -0600

    kata #67: Add Binary

---
+++ src/main/java/me/hermanliang/kata/math/AddBinary.java
@@ -0,0 +1,32 @@
+package me.hermanliang.kata.math;
+
+/**
+ * @see <a href="https://leetcode.com/problems/add-binary/description/">
+ * https://leetcode.com/problems/add-binary/description/</a>
+ */
+public class AddBinary {
+
+    /**
+     * 67. Add Binary
+     * <p>
+     * Given two binary strings, return their sum (also a binary string).
+     * <p>
+     * The input strings are both non-empty and contains only characters 1 or 0.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: a = "11", b = "1"
+     * Output: "100"
+     * Example 2:
+     * <p>
+     * Input: a = "1010", b = "1011"
+     * Output: "10101"
+     *
+     * @param a binary string a
+     * @param b binary string b
+     * @return a + b
+     */
+    public String addBinary(String a, String b) {
+        return null;
+    }
+}
