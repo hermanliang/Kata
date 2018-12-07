ac39f6fcb293f8d6873c44721b1934121a17b88e
Herman Liang <hermanliang80@gmail.com>
2018-12-07 05:04:55 -0600

    kata #33: Search in Rotated Sorted Array

---
+++ src/test/java/me/hermanliang/kata/array/RotatedSortedArrayTest.java
@@ -0,0 +1,24 @@
+package me.hermanliang.kata.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RotatedSortedArrayTest {
+
+    @Test
+    public void search1() {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int expected = 4;
+        assertEquals(expected, new RotatedSortedArray().search(nums, target));
+    }
+
+    @Test
+    public void search2() {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 3;
+        int expected = -1;
+        assertEquals(expected, new RotatedSortedArray().search(nums, target));
+    }
+}
