11acdde7ae07f1fc4090d53a42102b7cb1b0c87a
Herman Liang <hermanliang80@gmail.com>
2018-12-10 06:57:37 -0600

    kata #43: solution 11.40%

--- src/main/java/me/hermanliang/kata/math/MultiplyString.java
+++ src/main/java/me/hermanliang/kata/math/MultiplyString.java
@@ -32,6 +32,58 @@ public class MultiplyString {
      * @return product of num1 and num2
      */
     public String multiply(String num1, String num2) {
-        return null;
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        // always let num2.length >= num1.length
+        if (num2.length() < num1.length()) return multiply(num2, num1);
+        //       123
+        // x     456
+        // ---------
+        //       738
+        //      6150
+        //     49200
+        // ---------
+        //     56088
+        int i = num1.length() - 1;
+        String output = "0";
+        int exponent = 0;
+        while (i >= 0) {
+            StringBuilder tmp = new StringBuilder();
+            int carry = 0;
+            int j = num2.length() - 1;
+            while (j >= 0) {
+                int product = carry;
+                product += (num1.charAt(i) - '0') * (num2.charAt(j--) - '0');
+                carry = product / 10;
+                tmp.insert(0, product % 10);
+            }
+            if (carry > 0) {
+                tmp.insert(0, carry);
+            }
+            for (int k = 0; k < exponent; k++) {
+                tmp.append("0");
+            }
+            output = add(output, tmp.toString());
+            i--;
+            exponent++;
+        }
+        return output;
+    }
+
+    private String add(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += (num1.charAt(i--) - '0');
+            if (j >= 0) sum += (num2.charAt(j--) - '0');
+            carry = sum / 10;
+            sb.insert(0, sum % 10);
+        }
+        if (carry > 0) {
+            sb.insert(0, carry);
+        }
+        return sb.toString();
     }
 }
