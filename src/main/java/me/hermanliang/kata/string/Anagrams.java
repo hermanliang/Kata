faefdb4f7f2487656bc1f883dfaaed91988796ec
Herman Liang <hermanliang80@gmail.com>
2018-12-11 07:35:29 -0600

    kata #49: Group Anagrams

---
+++ src/main/java/me/hermanliang/kata/string/Anagrams.java
@@ -0,0 +1,36 @@
+package me.hermanliang.kata.string;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/group-anagrams/description/">
+ * https://leetcode.com/problems/group-anagrams/description/</a>
+ */
+public class Anagrams {
+
+    /**
+     * 49. Group Anagrams [Medium]
+     * <p>
+     * Given an array of strings, group anagrams together.
+     * <p>
+     * Example:
+     * <p>
+     * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+     * Output:
+     * [
+     * ["ate","eat","tea"],
+     * ["nat","tan"],
+     * ["bat"]
+     * ]
+     * Note:
+     * <p>
+     * All inputs will be in lowercase.
+     * The order of your output does not matter.
+     *
+     * @param strs array of strings
+     * @return list of group anagrams
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        return null;
+    }
+}
