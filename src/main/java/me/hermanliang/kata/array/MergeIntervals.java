8b7f10b585605773ebe33484f4baceafd57dd06a
Herman Liang <hermanliang80@gmail.com>
2018-12-13 06:27:41 -0600

    kata #56: Merge Intervals

---
+++ src/main/java/me/hermanliang/kata/array/MergeIntervals.java
@@ -0,0 +1,35 @@
+package me.hermanliang.kata.array;
+
+import me.hermanliang.kata.util.Interval;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/merge-intervals/description/">
+ * https://leetcode.com/problems/merge-intervals/description/</a>
+ */
+public class MergeIntervals {
+
+    /**
+     * 56. Merge Intervals [Medium]
+     * <p>
+     * Given a collection of intervals, merge all overlapping intervals.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: [[1,3],[2,6],[8,10],[15,18]]
+     * Output: [[1,6],[8,10],[15,18]]
+     * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+     * Example 2:
+     * <p>
+     * Input: [[1,4],[4,5]]
+     * Output: [[1,5]]
+     * Explanation: Intervals [1,4] and [4,5] are considerred overlapping.
+     *
+     * @param intervals intervals
+     * @return merged intervals
+     */
+    public List<Interval> merge(List<Interval> intervals) {
+        return null;
+    }
+}
---
+++ src/main/java/me/hermanliang/kata/util/Interval.java
@@ -0,0 +1,27 @@
+package me.hermanliang.kata.util;
+
+public class Interval {
+
+    public int start;
+    public int end;
+
+    public Interval() {
+        this.start = 0;
+        this.end = 0;
+    }
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Interval) {
+            Interval interval = (Interval) obj;
+            return this.start == interval.start && this.end == interval.end;
+        }
+        return super.equals(obj);
+    }
+}
+
