6cedf52f7039cace063efa6236e4b74244a86a15
Herman Liang <hermanliang80@gmail.com>
2018-12-18 07:31:03 -0600

    kata #79: solution 55.59%

--- src/main/java/me/hermanliang/kata/array/WordSearch.java
+++ src/main/java/me/hermanliang/kata/array/WordSearch.java
@@ -31,6 +31,48 @@ public class WordSearch {
      * @return the word is exist in the board
      */
     public boolean exist(char[][] board, String word) {
+        char c = word.charAt(0);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == c) {
+                    board[i][j] = '#';
+                    if (exist(board, word, i, j, 1)) return true;
+                    board[i][j] = c;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean exist(char[][] board, String word, int i, int j, int k) {
+        if (k == word.length()) {
+            return true;
+        }
+        char c = word.charAt(k++);
+        // left
+        if (j > 0 && board[i][j - 1] == c) {
+            board[i][j - 1] = '#';
+            if (exist(board, word, i, j - 1, k)) return true;
+            board[i][j - 1] = c;
+        }
+        // top
+        if (i > 0 && board[i - 1][j] == c) {
+            board[i - 1][j] = '#';
+            if (exist(board, word, i - 1, j, k)) return true;
+            board[i - 1][j] = c;
+        }
+        // right
+        if (j < board[0].length - 1 && board[i][j + 1] == c) {
+            board[i][j + 1] = '#';
+            if (exist(board, word, i, j + 1, k)) return true;
+            board[i][j + 1] = c;
+        }
+        // bottom
+        if (i < board.length - 1 && board[i + 1][j] == c) {
+            board[i + 1][j] = '#';
+            if (exist(board, word, i + 1, j, k)) return true;
+            board[i + 1][j] = c;
+        }
         return false;
     }
 }
