138e77c7a1f5f24cbf703c0c94cb482666292b41
Herman Liang <hermanliang80@gmail.com>
2018-12-27 06:08:28 -0600

    kata #121: solution 10.68%

--- src/main/java/me/hermanliang/kata/array/MaxProfit.java
+++ src/main/java/me/hermanliang/kata/array/MaxProfit.java
@@ -32,6 +32,12 @@ public class MaxProfit {
      * @return max profit
      */
     public int maxProfit(int[] prices) {
-        return 0;
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                profit = Math.max(profit, prices[j] - prices[i]);
+            }
+        }
+        return profit;
     }
 }
