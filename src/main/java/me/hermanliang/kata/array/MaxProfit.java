9a06c466d559b392e887a8f67d98d629ef2c6a78
Herman Liang <hermanliang80@gmail.com>
2018-12-28 06:14:34 -0600

    kata #121: rev 99.97%

--- src/main/java/me/hermanliang/kata/array/MaxProfit.java
+++ src/main/java/me/hermanliang/kata/array/MaxProfit.java
@@ -33,9 +33,12 @@ public class MaxProfit {
      */
     public int maxProfit(int[] prices) {
         int profit = 0;
-        for (int i = 0; i < prices.length - 1; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                profit = Math.max(profit, prices[j] - prices[i]);
+        int minPrice = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                profit = Math.max(profit, price - minPrice);
             }
         }
         return profit;
