2b576bc98abc9850936a440888f64895493c0fb1
Herman Liang <hermanliang80@gmail.com>
2019-01-03 06:17:24 -0600

    kata #139: Word Break

---
+++ src/main/java/me/hermanliang/kata/string/WordBreak.java
@@ -0,0 +1,44 @@
+package me.hermanliang.kata.string;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/word-break/description/">
+ * https://leetcode.com/problems/word-break/description/</a>
+ */
+public class WordBreak {
+
+    /**
+     * 139. Word Break [Medium]
+     * <p>
+     * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+     * determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+     * <p>
+     * Note:
+     * <p>
+     * The same word in the dictionary may be reused multiple times in the segmentation.
+     * You may assume the dictionary does not contain duplicate words.
+     * Example 1:
+     * <p>
+     * Input: s = "leetcode", wordDict = ["leet", "code"]
+     * Output: true
+     * Explanation: Return true because "leetcode" can be segmented as "leet code".
+     * Example 2:
+     * <p>
+     * Input: s = "applepenapple", wordDict = ["apple", "pen"]
+     * Output: true
+     * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+     * Note that you are allowed to reuse a dictionary word.
+     * Example 3:
+     * <p>
+     * Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+     * Output: false
+     *
+     * @param s        a string
+     * @param wordDict word dictionary
+     * @return the string can be segmented into a space-separated sequence of one or more dictionary words.
+     */
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return false;
+    }
+}
