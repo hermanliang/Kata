1fc080c67bd03107df369b2e9691baebe33b51a0
Herman Liang <hermanliang80@gmail.com>
2019-01-05 06:08:53 -0600

    kata #168: Excel Sheet Column Title

---
+++ src/main/java/me/hermanliang/kata/math/ExcelSheet.java
@@ -0,0 +1,43 @@
+package me.hermanliang.kata.math;
+
+/**
+ * @see <a href="https://leetcode.com/problems/excel-sheet-column-title/description/">
+ * https://leetcode.com/problems/excel-sheet-column-title/description/</a>
+ */
+public class ExcelSheet {
+
+    /**
+     * 168. Excel Sheet Column Title [Easy]
+     * <p>
+     * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+     * <p>
+     * For example:
+     * <p>
+     * 1 -> A
+     * 2 -> B
+     * 3 -> C
+     * ...
+     * 26 -> Z
+     * 27 -> AA
+     * 28 -> AB
+     * ...
+     * Example 1:
+     * <p>
+     * Input: 1
+     * Output: "A"
+     * Example 2:
+     * <p>
+     * Input: 28
+     * Output: "AB"
+     * Example 3:
+     * <p>
+     * Input: 701
+     * Output: "ZY"
+     *
+     * @param n a integer
+     * @return corresponding column title
+     */
+    public String convertToTitle(int n) {
+        return null;
+    }
+}
---
+++ src/test/java/me/hermanliang/kata/math/ExcelSheetTest.java
@@ -0,0 +1,29 @@
+package me.hermanliang.kata.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExcelSheetTest {
+
+    @Test
+    public void convertToTitle1() {
+        int n = 1;
+        String expected = "A";
+        assertEquals(expected, new ExcelSheet().convertToTitle(n));
+    }
+
+    @Test
+    public void convertToTitle2() {
+        int n = 28;
+        String expected = "AB";
+        assertEquals(expected, new ExcelSheet().convertToTitle(n));
+    }
+
+    @Test
+    public void convertToTitle3() {
+        int n = 701;
+        String expected = "ZY";
+        assertEquals(expected, new ExcelSheet().convertToTitle(n));
+    }
+}
