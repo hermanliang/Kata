b1a6270d6c9dbf2c424d66bdba9011d539e1e0aa
Herman Liang <hermanliang80@gmail.com>
2019-01-10 06:41:56 -0600

    kata #200: Number of Islands

---
+++ src/main/java/me/hermanliang/kata/array/NumberOfIslands.java
@@ -0,0 +1,41 @@
+package me.hermanliang.kata.array;
+
+/**
+ * @see <a href="https://leetcode.com/problems/number-of-islands/description/">
+ * https://leetcode.com/problems/number-of-islands/description/</a>
+ */
+public class NumberOfIslands {
+
+    /**
+     * 200. Number of Islands [Medium]
+     * <p>
+     * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+     * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+     * You may assume all four edges of the grid are all surrounded by water.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input:
+     * 11110
+     * 11010
+     * 11000
+     * 00000
+     * <p>
+     * Output: 1
+     * Example 2:
+     * <p>
+     * Input:
+     * 11000
+     * 11000
+     * 00100
+     * 00011
+     * <p>
+     * Output: 3
+     *
+     * @param grid a 2D grid map
+     * @return number of islands
+     */
+    public int numIslands(char[][] grid) {
+        return 0;
+    }
+}
