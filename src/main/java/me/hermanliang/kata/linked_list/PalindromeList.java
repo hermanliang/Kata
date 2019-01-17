1403e2be480024dfa032f93b856ce05b6040ec01
Herman Liang <hermanliang80@gmail.com>
2019-01-17 06:14:22 -0600

    kata #234: Palindrome Linked List

---
+++ src/main/java/me/hermanliang/kata/linked_list/PalindromeList.java
@@ -0,0 +1,33 @@
+package me.hermanliang.kata.linked_list;
+
+import me.hermanliang.kata.util.ListNode;
+
+/**
+ * @see <a href="https://leetcode.com/problems/palindrome-linked-list/description/">
+ * https://leetcode.com/problems/palindrome-linked-list/description/</a>
+ */
+public class PalindromeList {
+
+    /**
+     * 234. Palindrome Linked List [Easy]
+     * <p>
+     * Given a singly linked list, determine if it is a palindrome.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: 1->2
+     * Output: false
+     * Example 2:
+     * <p>
+     * Input: 1->2->2->1
+     * Output: true
+     * Follow up:
+     * Could you do it in O(n) time and O(1) space?
+     *
+     * @param head a ListNode
+     * @return is the ListNode a palindrome
+     */
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
+}
---
+++ src/test/java/me/hermanliang/kata/linked_list/PalindromeListTest.java
@@ -0,0 +1,23 @@
+package me.hermanliang.kata.linked_list;
+
+import me.hermanliang.kata.util.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PalindromeListTest {
+
+    @Test
+    public void isPalindrome1() {
+        ListNode head = ListNode.arrayToListNode(new int[]{1, 2});
+        boolean expected = false;
+        assertEquals(expected, new PalindromeList().isPalindrome(head));
+    }
+
+    @Test
+    public void isPalindrome2() {
+        ListNode head = ListNode.arrayToListNode(new int[]{1, 2, 2, 1});
+        boolean expected = true;
+        assertEquals(expected, new PalindromeList().isPalindrome(head));
+    }
+}
