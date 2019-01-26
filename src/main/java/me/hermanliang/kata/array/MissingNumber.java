d89c7639f8cc0f6d6c0ed22357ed5bbcb990c96b
Herman Liang <hermanliang80@gmail.com>
2019-01-26 06:36:05 -0600

    kata #268: Missing Number

---
+++ src/main/java/me/hermanliang/kata/array/MissingNumber.java
@@ -0,0 +1,33 @@
+package me.hermanliang.kata.array;
+
+/**
+ * @see <a href="https://leetcode.com/problems/missing-number/description/">
+ * https://leetcode.com/problems/missing-number/description/</a>
+ */
+public class MissingNumber {
+
+    /**
+     * 268. Missing Number [Easy]
+     * <p>
+     * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n,
+     * find the one that is missing from the array.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: [3,0,1]
+     * Output: 2
+     * Example 2:
+     * <p>
+     * Input: [9,6,4,2,3,5,7,0,1]
+     * Output: 8
+     * Note:
+     * Your algorithm should run in linear runtime complexity.
+     * Could you implement it using only constant extra space complexity?
+     *
+     * @param nums an integer array
+     * @return the missing number
+     */
+    public int missingNumber(int[] nums) {
+        return 0;
+    }
+}
---
+++ src/test/java/me/hermanliang/kata/array/MissingNumberTest.java
@@ -0,0 +1,22 @@
+package me.hermanliang.kata.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MissingNumberTest {
+
+    @Test
+    public void missingNumber1() {
+        int[] nums = {3, 0, 1};
+        int expected = 2;
+        assertEquals(expected, new MissingNumber().missingNumber(nums));
+    }
+
+    @Test
+    public void missingNumber2() {
+        int[] nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        int expected = 8;
+        assertEquals(expected, new MissingNumber().missingNumber(nums));
+    }
+}
