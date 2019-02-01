639e226d0e13dbd57161d145985255c2df4a8f73
Herman Liang <hermanliang80@gmail.com>
2019-02-01 06:28:45 -0600

    kata #461: Hamming Distance

---
+++ src/test/java/me/hermanliang/kata/math/HammingDistanceTest.java
@@ -0,0 +1,16 @@
+package me.hermanliang.kata.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HammingDistanceTest {
+
+    @Test
+    public void hammingDistance() {
+        int x = 1;
+        int y = 4;
+        int expected = 2;
+        assertEquals(expected, new HammingDistance().hammingDistance(x, y));
+    }
+}
