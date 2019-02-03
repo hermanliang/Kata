9d2fb6c5272da0df9017c6ec0abda0a35e11e544
Herman Liang <hermanliang80@gmail.com>
2019-02-03 06:35:42 -0600

    kata #647: Palindromic Substrings

---
+++ src/main/java/me/hermanliang/kata/string/PalindromicSubstrings.java
@@ -0,0 +1,37 @@
+package me.hermanliang.kata.string;
+
+/**
+ * @see <a href="https://leetcode.com/problems/palindromic-substrings/description/">
+ * https://leetcode.com/problems/palindromic-substrings/description/</a>
+ */
+public class PalindromicSubstrings {
+
+    /**
+     * 647. Palindromic Substrings [Medium]
+     * <p>
+     * Given a string, your task is to count how many palindromic substrings in this string.
+     * <p>
+     * The substrings with different start indexes or end indexes are counted as different
+     * substrings even they consist of same characters.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: "abc"
+     * Output: 3
+     * Explanation: Three palindromic strings: "a", "b", "c".
+     * Example 2:
+     * <p>
+     * Input: "aaa"
+     * Output: 6
+     * Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+     * Note:
+     * <p>
+     * The input string length won't exceed 1000.
+     *
+     * @param s a string
+     * @return number of palindromic substrings in the string
+     */
+    public int countSubstrings(String s) {
+        return 0;
+    }
+}
