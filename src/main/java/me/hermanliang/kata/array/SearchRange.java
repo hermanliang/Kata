e5df49e05ac667f1b51ffded3575a61a86b2d5bd
Herman Liang <hermanliang80@gmail.com>
2019-02-06 06:22:07 -0600

    kata #34: solution 100%

--- src/main/java/me/hermanliang/kata/array/SearchRange.java
+++ src/main/java/me/hermanliang/kata/array/SearchRange.java
@@ -1,5 +1,7 @@
 package me.hermanliang.kata.array;
 
+import java.util.Arrays;
+
 /**
  * @see <a href="https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/">
  * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/</a>
@@ -30,6 +32,54 @@ public class SearchRange {
      * @return starting end ending position of the target value
      */
     public int[] searchRange(int[] nums, int target) {
-        return new int[0];
+        int[] output = new int[2];
+        Arrays.fill(output, -1);
+        int start = 0;
+        int end = nums.length - 1;
+        while (end >= start) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                output[0] = searchStart(nums, target, start, mid);
+                output[1] = searchEnd(nums, target, mid, end);
+                break;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return output;
+    }
+
+    private int searchStart(int[] nums, int target, int start, int end) {
+        if (end == 0 || nums[end - 1] != target) return end;
+        end--;
+        while (end >= start) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return searchStart(nums, target, start, mid);
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    private int searchEnd(int[] nums, int target, int start, int end) {
+        if (start == (nums.length - 1) || nums[start + 1] != target) return start;
+        start++;
+        while (end >= start) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return searchEnd(nums, target, mid, end);
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
     }
 }
