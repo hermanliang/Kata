7ef108f6c0d8f9115f6581d03d24bf402397d5e1
Herman Liang <hermanliang80@gmail.com>
2019-02-09 06:55:31 -0600

    kata #39: Combination Sum

---
+++ src/main/java/me/hermanliang/kata/array/CombinationSum.java
@@ -0,0 +1,48 @@
+package me.hermanliang.kata.array;
+
+import java.util.List;
+
+/**
+ * @see <a href="https://leetcode.com/problems/combination-sum/description/">
+ * https://leetcode.com/problems/combination-sum/description/</a>
+ */
+public class CombinationSum {
+
+    /**
+     * 39. Combination Sum [Medium]
+     * <p>
+     * Given a set of candidate numbers (candidates) (without duplicates) and a target number (target),
+     * find all unique combinations in candidates where the candidate numbers sums to target.
+     * <p>
+     * The same repeated number may be chosen from candidates unlimited number of times.
+     * <p>
+     * Note:
+     * <p>
+     * All numbers (including target) will be positive integers.
+     * The solution set must not contain duplicate combinations.
+     * Example 1:
+     * <p>
+     * Input: candidates = [2,3,6,7], target = 7,
+     * A solution set is:
+     * [
+     * [7],
+     * [2,2,3]
+     * ]
+     * Example 2:
+     * <p>
+     * Input: candidates = [2,3,5], target = 8,
+     * A solution set is:
+     * [
+     * [2,2,2,2],
+     * [2,3,3],
+     * [3,5]
+     * ]
+     *
+     * @param candidates candidates
+     * @param target     target
+     * @return output
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return null;
+    }
+}
