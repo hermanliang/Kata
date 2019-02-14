860e76ede56a6ff1845cb18a5da13da62f636ad9
Herman Liang <hermanliang80@gmail.com>
2019-02-14 06:38:15 -0600

    kata #53: Maximum Subarray

---
+++ src/test/java/me/hermanliang/kata/array/MaxSubArrayTest.java
@@ -0,0 +1,15 @@
+package me.hermanliang.kata.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaxSubArrayTest {
+
+    @Test
+    public void maxSubArray() {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int expected = 6;
+        assertEquals(expected, new MaxSubArray().maxSubArray(nums));
+    }
+}
