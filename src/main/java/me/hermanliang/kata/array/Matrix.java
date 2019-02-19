3863882c59876449971af5e6ecc13fb97598c553
Herman Liang <hermanliang80@gmail.com>
2019-02-19 06:58:47 -0600

    kata #73: solution 11.79%

--- src/main/java/me/hermanliang/kata/array/Matrix.java
+++ src/main/java/me/hermanliang/kata/array/Matrix.java
@@ -1,5 +1,8 @@
 package me.hermanliang.kata.array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @see <a href="https://leetcode.com/problems/set-matrix-zeroes/">
  * https://leetcode.com/problems/set-matrix-zeroes/</a>
@@ -48,6 +51,41 @@ public class Matrix {
      * @param matrix a matrix
      */
     public void setZeroes(int[][] matrix) {
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0 && !rows.contains(i) && !cols.contains(j)) {
+                    rows.add(i);
+                    cols.add(j);
+                    setRowZeroes(matrix, i, rows, cols);
+                    setColZeroes(matrix, j, rows, cols);
+                }
+            }
+        }
+    }
 
+    private void setRowZeroes(int[][] matrix, int row, Set<Integer> rows, Set<Integer> cols) {
+        for (int i = 0; i < matrix[row].length; i++) {
+            if (matrix[row][i] == 0 && !cols.contains(i)) {
+                cols.add(i);
+                setColZeroes(matrix, i, rows, cols);
+            } else {
+                matrix[row][i] = 0;
+            }
+        }
+    }
+
+    private void setColZeroes(int[][] matrix, int col, Set<Integer> rows, Set<Integer> cols) {
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][col] == 0 && !rows.contains(i)) {
+                rows.add(i);
+                setRowZeroes(matrix, i, rows, cols);
+            } else {
+                matrix[i][col] = 0;
+            }
+        }
     }
 }
