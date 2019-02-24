ede11cd51f20f87e9d78bddd155f5ba3744b5ca7
Herman Liang <hermanliang80@gmail.com>
2019-02-24 06:32:57 -0600

    kata #75. Sort Colors

---
+++ src/test/java/me/hermanliang/kata/array/SortColorsTest.java
@@ -0,0 +1,16 @@
+package me.hermanliang.kata.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SortColorsTest {
+
+    @Test
+    public void sortColors() {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        int[] expected = {0, 0, 1, 1, 2, 2};
+        new SortColors().sortColors(nums);
+        assertArrayEquals(expected, nums);
+    }
+}
