42af604775c5c1c60d974d3e676cc5a6b155db8d
Herman Liang <hermanliang80@gmail.com>
2019-02-25 06:34:22 -0600

    kata #75: solution 100%

--- src/main/java/me/hermanliang/kata/array/SortColors.java
+++ src/main/java/me/hermanliang/kata/array/SortColors.java
@@ -30,6 +30,18 @@ public class SortColors {
      * @param nums an integer array
      */
     public void sortColors(int[] nums) {
-
+        int[] count = new int[3];
+        for (int num : nums) {
+            count[num]++;
+        }
+        for (int i = 0; i < count[0]; i++) {
+            nums[i] = 0;
+        }
+        for (int i = 0; i < count[1]; i++) {
+            nums[i + count[0]] = 1;
+        }
+        for (int i = 0; i < count[2]; i++) {
+            nums[i + count[0] + count[1]] = 2;
+        }
     }
 }
