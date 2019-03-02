01daeac4c0da7b2491d96d08fcd42685a2abeaba
Herman Liang <hermanliang80@gmail.com>
2019-03-02 06:22:26 -0600

    kata #98. Validate Binary Search Tree

--- src/main/java/me/hermanliang/kata/tree/BinarySearchTree.java
+++ src/main/java/me/hermanliang/kata/tree/BinarySearchTree.java
@@ -5,6 +5,8 @@ import me.hermanliang.kata.util.TreeNode;
 /**
  * @see <a href="https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/">
  * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/</a>
+ * @see <a href="https://leetcode.com/problems/validate-binary-search-tree/">
+ * https://leetcode.com/problems/validate-binary-search-tree/</a>
  */
 public class BinarySearchTree {
 
@@ -49,5 +51,39 @@ public class BinarySearchTree {
             if (q.val > root.val) return root;
             return lowestCommonAncestor(root.left, p, q);
         }
+    }
+
+    /**
+     * 98. Validate Binary Search Tree [Medium]
+     *
+     * Given a binary tree, determine if it is a valid binary search tree (BST).
+     *
+     * Assume a BST is defined as follows:
+     *
+     * The left subtree of a node contains only nodes with keys less than the node's key.
+     * The right subtree of a node contains only nodes with keys greater than the node's key.
+     * Both the left and right subtrees must also be binary search trees.
+     * Example 1:
+     *
+     * Input:
+     *     2
+     *    / \
+     *   1   3
+     * Output: true
+     * Example 2:
+     *
+     *     5
+     *    / \
+     *   1   4
+     *      / \
+     *     3   6
+     * Output: false
+     * Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+     *              is 5 but its right child's value is 4.
+     * @param root a TreeNode
+     * @return is the TreeNode a valid BST
+     */
+    public boolean isValidBST(TreeNode root) {
+        return false;
     }
 }
--- src/test/java/me/hermanliang/kata/tree/BinarySearchTreeTest.java
+++ src/test/java/me/hermanliang/kata/tree/BinarySearchTreeTest.java
@@ -3,7 +3,7 @@ package me.hermanliang.kata.tree;
 import me.hermanliang.kata.util.TreeNode;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class BinarySearchTreeTest {
 
@@ -23,5 +23,17 @@ public class BinarySearchTreeTest {
         TreeNode q = p.right;
         TreeNode expected = p;
         assertEquals(expected, new BinarySearchTree().lowestCommonAncestor(root, p, q));
+    }
+
+    @Test
+    public void isValidBST1() {
+        TreeNode root = TreeNode.arrayToTreeNode(new Integer[]{2, 1, 3});
+        assertTrue(new BinarySearchTree().isValidBST(root));
+    }
+
+    @Test
+    public void isValidBST2() {
+        TreeNode root = TreeNode.arrayToTreeNode(new Integer[]{5, 1, 4, null, null, 3, 6});
+        assertFalse(new BinarySearchTree().isValidBST(root));
     }
 }
