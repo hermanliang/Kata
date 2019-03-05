77fe65ee26a1e4e0e3e119c6b24f07129d03e3f7
Herman Liang <hermanliang80@gmail.com>
2019-03-05 07:02:28 -0600

    kata #81: solution 100%

--- src/main/java/me/hermanliang/kata/array/RotatedSortedArray.java
+++ src/main/java/me/hermanliang/kata/array/RotatedSortedArray.java
@@ -99,6 +99,30 @@ public class RotatedSortedArray {
      * @return has target
      */
     public boolean searchWithDuplicate(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (end >= start) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return true;
+            else if (nums[start] == nums[mid] && nums[end] == nums[mid]) end--;
+            else if (nums[mid] > target) {
+                if (nums[start] <= target) { // left side is sorted array
+                    return binarySearch(nums, start, mid - 1, target) != -1;
+                } else if (nums[start] > nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (nums[end] >= target) {
+                    return binarySearch(nums, mid + 1, end, target) != -1;
+                } else if (nums[end] < nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
         return false;
     }
 }
