1cb72f0d43f578a770e0091595ec0f9fcb53e92d
Herman Liang <hermanliang80@gmail.com>
2019-03-07 07:12:49 -0600

    kata #100: solution 98.66%

--- src/main/java/me/hermanliang/kata/tree/BinaryTree.java
+++ src/main/java/me/hermanliang/kata/tree/BinaryTree.java
@@ -153,6 +153,11 @@ public class BinaryTree {
      * @return is same tree
      */
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
         return false;
     }
 }
