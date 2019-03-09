418e8a485dbd5ec6bf13c323df7642a5284d4d1a
Herman Liang <hermanliang80@gmail.com>
2019-03-09 07:19:38 -0600

    kata #101: solution 98.37%

--- src/main/java/me/hermanliang/kata/tree/BinaryTree.java
+++ src/main/java/me/hermanliang/kata/tree/BinaryTree.java
@@ -191,6 +191,16 @@ public class BinaryTree {
      * @return is symmetric tree
      */
     public boolean isSymmetric(TreeNode root) {
-        return false;
+        if (root == null) return true;
+        if (root.left == null && root.right == null) return true;
+        if (root.left == null || root.right == null) return false;
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 }
