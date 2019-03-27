887ccdda2efe42e9e05ef0fa09fcdd7e614c43b9
Herman Liang <hermanliang80@gmail.com>
2019-03-27 06:25:56 -0600

    kata #122: solution 99.05%

--- src/main/java/me/hermanliang/kata/array/MaxProfit.java
+++ src/main/java/me/hermanliang/kata/array/MaxProfit.java
@@ -80,6 +80,11 @@ public class MaxProfit {
      * @return
      */
     public int maxProfitWithMultipleTransactions(int[] prices) {
-        return 0;
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
     }
 }
