34cac726cee00a17e38b4f51919e024bdf1d9307
Herman Liang <hermanliang80@gmail.com>
2019-04-01 05:57:09 -0600

    kata #137. Single Number II

--- src/main/java/me/hermanliang/kata/math/SingleNumber.java
+++ src/main/java/me/hermanliang/kata/math/SingleNumber.java
@@ -3,6 +3,8 @@ package me.hermanliang.kata.math;
 /**
  * @see <a href="https://leetcode.com/problems/single-number/">
  * https://leetcode.com/problems/single-number/</a>
+ * @see <a href="https://leetcode.com/problems/single-number-ii/">
+ * https://leetcode.com/problems/single-number-ii/</a>
  */
 public class SingleNumber {
 
@@ -33,5 +35,31 @@ public class SingleNumber {
             result ^= n;
         }
         return result;
+    }
+
+    /**
+     * 137. Single Number II [Medium]
+     * <p>
+     * Given a non-empty array of integers, every element appears three times except for one, which appears exactly once.
+     * Find that single one.
+     * <p>
+     * Note:
+     * <p>
+     * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: [2,2,3,2]
+     * Output: 3
+     * Example 2:
+     * <p>
+     * Input: [0,1,0,1,0,1,99]
+     * Output: 99
+     *
+     * @param nums an integer array
+     * @return the single number
+     */
+    public int singleNumberWithThree(int[] nums) {
+        return 0;
     }
 }
--- src/test/java/me/hermanliang/kata/math/SingleNumberTest.java
+++ src/test/java/me/hermanliang/kata/math/SingleNumberTest.java
@@ -19,4 +19,18 @@ public class SingleNumberTest {
         int expected = 4;
         assertEquals(expected, new SingleNumber().singleNumber(nums));
     }
+
+    @Test
+    public void singleNumberWithThree1() {
+        int[] nums = {2, 2, 3, 2};
+        int expected = 3;
+        assertEquals(expected, new SingleNumber().singleNumberWithThree(nums));
+    }
+
+    @Test
+    public void singleNumberWithThree2() {
+        int[] nums = {0, 1, 0, 1, 0, 1, 99};
+        int expected = 99;
+        assertEquals(expected, new SingleNumber().singleNumberWithThree(nums));
+    }
 }
