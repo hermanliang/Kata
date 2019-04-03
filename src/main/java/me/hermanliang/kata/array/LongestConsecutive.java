278d24b6bf9615303a8b102342e52a67271967f4
Herman Liang <hermanliang80@gmail.com>
2019-04-03 06:28:45 -0600

    kata #128. Longest Consecutive Sequence

---
+++ src/main/java/me/hermanliang/kata/array/LongestConsecutive.java
@@ -0,0 +1,28 @@
+package me.hermanliang.kata.array;
+
+/**
+ * @see <a href="https://leetcode.com/problems/longest-consecutive-sequence/">
+ * https://leetcode.com/problems/longest-consecutive-sequence/</a>
+ */
+public class LongestConsecutive {
+
+    /**
+     * 128. Longest Consecutive Sequence [Hard]
+     * <p>
+     * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+     * <p>
+     * Your algorithm should run in O(n) complexity.
+     * <p>
+     * Example:
+     * <p>
+     * Input: [100, 4, 200, 1, 3, 2]
+     * Output: 4
+     * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+     *
+     * @param nums an integer array
+     * @return largest consecutive sequence
+     */
+    public int longestConsecutive(int[] nums) {
+        return 0;
+    }
+}
---
+++ src/test/java/me/hermanliang/kata/array/LongestConsecutiveTest.java
@@ -0,0 +1,15 @@
+package me.hermanliang.kata.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LongestConsecutiveTest {
+
+    @Test
+    public void longestConsecutive() {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int expected = 4;
+        assertEquals(expected, new LongestConsecutive().longestConsecutive(nums));
+    }
+}
