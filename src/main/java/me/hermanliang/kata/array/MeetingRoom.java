31c0063690cf38d070726e8daedf33d039b55f17
Herman Liang <hermanliang80@gmail.com>
2019-04-05 06:01:24 -0600

    kata #253. Meeting Rooms II

---
+++ src/main/java/me/hermanliang/kata/array/MeetingRoom.java
@@ -0,0 +1,32 @@
+package me.hermanliang.kata.array;
+
+import me.hermanliang.kata.util.Interval;
+
+/**
+ * @see <a href="https://leetcode.com/problems/meeting-rooms-ii/">
+ * https://leetcode.com/problems/meeting-rooms-ii/</a>
+ */
+public class MeetingRoom {
+
+    /**
+     * 253. Meeting Rooms II [Medium]
+     * <p>
+     * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+     * find the minimum number of conference rooms required.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: [[0, 30],[5, 10],[15, 20]]
+     * Output: 2
+     * Example 2:
+     * <p>
+     * Input: [[7,10],[2,4]]
+     * Output: 1
+     *
+     * @param intervals time intervals
+     * @return minimum rooms
+     */
+    public int minMeetingRooms(Interval[] intervals) {
+        return 0;
+    }
+}
