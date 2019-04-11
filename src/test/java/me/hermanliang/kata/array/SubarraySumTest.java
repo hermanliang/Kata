96cb8eb935af9a1aabb20a70c5ee10f616254952
Herman Liang <hermanliang80@gmail.com>
2019-04-11 06:03:26 -0600

    kata #523. Continuous Subarray Sum

--- src/test/java/me/hermanliang/kata/array/SubarraySumTest.java
+++ src/test/java/me/hermanliang/kata/array/SubarraySumTest.java
@@ -13,4 +13,22 @@ public class SubarraySumTest {
         int expected = 2;
         assertEquals(expected, new SubarraySum().subarraySum(nums, k));
     }
+
+    @Test
+    public void checkSubarraySum1() {
+        int[] nums = {23, 2, 4, 6, 7};
+        int k = 6;
+        boolean expected = true;
+        // Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+        assertEquals(expected, new SubarraySum().checkSubarraySum(nums, k));
+    }
+
+    @Test
+    public void checkSubarraySum2() {
+        int[] nums = {23, 2, 6, 4, 7};
+        int k = 6;
+        boolean expected = true;
+        // Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+        assertEquals(expected, new SubarraySum().checkSubarraySum(nums, k));
+    }
 }
