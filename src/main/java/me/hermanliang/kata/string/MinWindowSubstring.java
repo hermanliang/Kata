d7a51b07a05d362ae1b110d661b8554610aa56f6
Herman Liang <hermanliang80@gmail.com>
2019-04-21 06:51:39 -0600

    kata #76: solution 60.29%

--- src/main/java/me/hermanliang/kata/string/MinWindowSubstring.java
+++ src/main/java/me/hermanliang/kata/string/MinWindowSubstring.java
@@ -1,5 +1,8 @@
 package me.hermanliang.kata.string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @see <a href="https://leetcode.com/problems/minimum-window-substring/">
  * https://leetcode.com/problems/minimum-window-substring/</a>
@@ -26,6 +29,39 @@ public class MinWindowSubstring {
    * window in S.
    */
   public String minWindow(String s, String t) {
-    return null;
+    Map<Character, Integer> map = getFeature(t);
+    int count = 0;
+    int left = 0;
+    int minLen = Integer.MAX_VALUE;
+    int minLeft = 0;
+    for (int right = 0; right < s.length(); right++) {
+      char c = s.charAt(right);
+      if (map.containsKey(c)) {
+        if (map.get(c) > 0) count++;
+        map.put(c, map.get(c) - 1);
+      }
+      while (count == t.length()) {
+        if ((right - left + 1) < minLen) {
+          minLen = right - left + 1;
+          minLeft = left;
+        }
+        char l = s.charAt(left);
+        if (map.containsKey(l)) {
+          map.put(l, map.get(l) + 1);
+          if (map.get(l) > 0) count--;
+        }
+        left++;
+      }
+    }
+    if (minLeft + minLen > s.length()) return "";
+    return s.substring(minLeft, minLeft + minLen);
+  }
+
+  private Map<Character, Integer> getFeature(String s) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (char c : s.toCharArray()) {
+      map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+    return map;
   }
 }
