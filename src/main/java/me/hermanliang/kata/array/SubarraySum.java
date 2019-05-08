ff8697da76d737c3c459048247a6a922207d90d2
Herman Liang <hermanliang80@gmail.com>
2019-05-08 20:19:56 +0800

    kata #560: solution 34.2%

--- src/main/java/me/hermanliang/kata/array/SubarraySum.java
+++ src/main/java/me/hermanliang/kata/array/SubarraySum.java
@@ -1,5 +1,8 @@
 package me.hermanliang.kata.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @see <a href="https://leetcode.com/problems/subarray-sum-equals-k/">
  * https://leetcode.com/problems/subarray-sum-equals-k/</a>
@@ -27,13 +30,15 @@ public class SubarraySum {
    */
   public int subarraySum(int[] nums, int k) {
     int count = 0;
-    for (int i = 0; i < nums.length; i++) {
-      int sum = nums[i];
-      if (sum == k) count++;
-      for (int j = i + 1; j < nums.length; j++) {
-        sum += nums[j];
-        if (sum == k) count++;
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(0, 1);
+    int sum = 0;
+    for (int n : nums) {
+      sum += n;
+      if (map.containsKey(sum - k)) {
+        count += map.get(sum - k);
       }
+      map.put(sum, map.getOrDefault(sum, 0) + 1);
     }
     return count;
   }
