7045171dd3a32bcc961e917d93c8319faf303230
Herman Liang <hermanliang80@gmail.com>
2019-05-14 20:18:42 +0800

    kata #567: solution 31.45%

--- src/main/java/me/hermanliang/kata/string/PermutationInString.java
+++ src/main/java/me/hermanliang/kata/string/PermutationInString.java
@@ -1,5 +1,8 @@
 package me.hermanliang.kata.string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @see <a href="https://leetcode.com/problems/permutation-in-string/">
  * https://leetcode.com/problems/permutation-in-string/</a>
@@ -37,6 +40,28 @@ public class PermutationInString {
    * @return return true if s2 contains the permutation of s1
    */
   public boolean checkInclusion(String s1, String s2) {
+    Map<Character, Integer> map = new HashMap<>();
+    for (char c : s1.toCharArray()) {
+      map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+    int count = 0;
+    int left = 0;
+    for (int right = 0; right < s2.length(); right++) {
+      char r = s2.charAt(right);
+      if (map.containsKey(r)) {
+        if (map.get(r) > 0) count++;
+        map.put(r, map.get(r) - 1);
+      }
+      while (count == s1.length()) {
+        if (right - left + 1 == s1.length()) return true;
+        char l = s2.charAt(left);
+        if (map.containsKey(l)) {
+          map.put(l, map.get(l) + 1);
+          if (map.get(l) > 0) count--;
+        }
+        left++;
+      }
+    }
     return false;
   }
 }
