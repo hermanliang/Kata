b408457e9f116168f75d9b16f0a2a2076b155df1
Herman Liang <hermanliang80@gmail.com>
2019-05-17 20:30:55 +0800

    kata #98. Validate Binary Search Tree

--- src/test/java/me/hermanliang/kata/tree/BinaryTreeTest.java
+++ src/test/java/me/hermanliang/kata/tree/BinaryTreeTest.java
@@ -237,4 +237,18 @@ public class BinaryTreeTest {
     int expected = 3;
     assertEquals(expected, new BinaryTree().diameterOfBinaryTree(root));
   }
+
+  @Test
+  public void isValidBST1() {
+    TreeNode root = TreeNode.arrayToTreeNode(new Integer[] {2, 1, 3});
+    boolean expected = true;
+    assertEquals(expected, new BinaryTree().isValidBST(root));
+  }
+
+  @Test
+  public void isValidBST2() {
+    TreeNode root = TreeNode.arrayToTreeNode(new Integer[] {5, 1, 4, null, null, 3, 6});
+    boolean expected = false;
+    assertEquals(expected, new BinaryTree().isValidBST(root));
+  }
 }
