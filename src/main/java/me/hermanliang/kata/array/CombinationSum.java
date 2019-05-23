b203b9d16cce7e03ae6b8d4bf5afe099b9aa109f
Herman Liang <hermanliang80@gmail.com>
2019-05-23 20:40:15 +0800

    kata #40. Combination Sum II

--- src/main/java/me/hermanliang/kata/array/CombinationSum.java
+++ src/main/java/me/hermanliang/kata/array/CombinationSum.java
@@ -9,6 +9,8 @@ import java.util.Queue;
 /**
  * @see <a href="https://leetcode.com/problems/combination-sum/description/">
  * https://leetcode.com/problems/combination-sum/description/</a>
+ * @see <a href="https://leetcode.com/problems/combination-sum-ii/">
+ * https://leetcode.com/problems/combination-sum-ii/</a>
  */
 public class CombinationSum {
 
@@ -59,5 +61,58 @@ public class CombinationSum {
       }
     }
     return result;
+  }
+
+  /**
+   * 40. Combination Sum II [Medium]
+   *
+   * Given a collection of candidate numbers (candidates) and a target number (target), find all
+   * unique combinations in candidates where the candidate numbers sums to target.
+   *
+   * Each number in candidates may only be used once in the combination.
+   *
+   * Note:
+   *
+   * All numbers (including target) will be positive integers. The solution set must not contain
+   * duplicate combinations.
+   *
+   * Example 1:
+   *
+   * Input: candidates = [10,1,2,7,6,1,5], target = 8,
+   *
+   * A solution set is:
+   *
+   * [
+   *
+   * [1, 7],
+   *
+   * [1, 2, 5],
+   *
+   * [2, 6],
+   *
+   * [1, 1, 6]
+   *
+   * ]
+   *
+   * Example 2:
+   *
+   * Input: candidates = [2,5,2,1,2], target = 5,
+   *
+   * A solution set is:
+   *
+   * [
+   *
+   * [1,2,2],
+   *
+   * [5]
+   *
+   * ]
+   *
+   * @param candidates candidates
+   * @param target target
+   * @return output
+   */
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    return null;
   }
 }
--- src/test/java/me/hermanliang/kata/array/CombinationSumTest.java
+++ src/test/java/me/hermanliang/kata/array/CombinationSumTest.java
@@ -1,5 +1,6 @@
 package me.hermanliang.kata.array;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -8,11 +9,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CombinationSumTest {
 
   @Test
-  public void combinationSum1() {
+  public void combinationSum1_1() {
     int[] candidates = {2, 3, 6, 7};
     int target = 7;
     Set<String> visited = new HashSet<>();
@@ -33,7 +35,7 @@ public class CombinationSumTest {
   }
 
   @Test
-  public void combinationSum2() {
+  public void combinationSum1_2() {
     int[] candidates = {2, 3, 5};
     int target = 8;
     Set<String> visited = new HashSet<>();
@@ -51,5 +53,37 @@ public class CombinationSumTest {
       assertFalse(visited.contains(sb.toString()));
       visited.add(sb.toString());
     }
+  }
+
+  @Test
+  public void combinationSum2_1() {
+    int[] candidates = {10, 1, 2, 7, 6, 1, 5};
+    int target = 8;
+    List<List<Integer>> expected = Arrays.asList(
+        Arrays.asList(1, 7),
+        Arrays.asList(1, 2, 5),
+        Arrays.asList(2, 6),
+        Arrays.asList(1, 1, 6));
+    List<List<Integer>> output = new CombinationSum().combinationSum2(candidates, target);
+    for (List<Integer> item : expected) {
+      assertTrue(output.contains(item));
+      output.remove(item);
+    }
+    assertTrue(output.isEmpty());
+  }
+
+  @Test
+  public void combinationSum2_2() {
+    int[] candidates = {2, 5, 2, 1, 2};
+    int target = 5;
+    List<List<Integer>> expected = Arrays.asList(
+        Arrays.asList(1, 2, 2),
+        Collections.singletonList(5));
+    List<List<Integer>> output = new CombinationSum().combinationSum2(candidates, target);
+    for (List<Integer> item : expected) {
+      assertTrue(output.contains(item));
+      output.remove(item);
+    }
+    assertTrue(output.isEmpty());
   }
 }
